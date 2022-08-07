7b7f06915cb815fbf9c620afefe0e77db041c6a6
Paul Okoli <75359053+PaulPextra@users.noreply.github.com>
2022-08-07 01:31:24 +0100

    Update print_c.c
    Updating print_c.c

--- print_c.c
+++ print_c.c
@@ -4,12 +4,12 @@
 /**
  * print_c - A function that prints a char
  * @c: character to print
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int print_c(va_list c)
 {
 	char character = (char)va_arg(c, int);
 
 	_putchar(character);
-	return (1);
+	return (0);
 }
