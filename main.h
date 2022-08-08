bf6462ea9ee3b85c63da7dde467653e757515128
Paul Okoli <75359053+PaulPextra@users.noreply.github.com>
2022-08-08 13:46:36 +0100

    Updating main.h file

--- main.h
+++ main.h
@@ -3,8 +3,6 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
-
-<<<<<<< HEAD
 /**
  * struct flags - struct containing flags to "turn on"
  * when a flag specifier is passed to _printf()
@@ -19,27 +17,36 @@ typedef struct flags
 	int hash;
 } flags_t;
 
-
 /**
  * struct printHandler - struct to choose the right function depending
  * on the format specifier passed to _printf()
- * @t: format specifier
+ * @c: format specifier
  * @f: pointer to the correct printing function
  */
-typedef struct print
+typedef struct printHandler
 {
-  char *t;
-  int (*f)(va_list);
-}print_t;
-
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+char *convert(unsigned long int num, int base, int lowercase);
 int _printf(const char *format, ...);
-int _puts(char *str);
-int _putchar(char c);
+int (*get_print(char s))(va_list, flags_t *);
+int get_flag(char s, flags_t *f);
 int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
-=======
 int _putchar(char c);
+int _puts(char *str);
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+int print_address(va_list l, flags_t *f);
 int print_percent(va_list l, flags_t *f);
-  
 #endif /* MAIN_H */
->>>>>>> ca6171d76717ef9ce5cd19a8ac84eb9c443dd562
