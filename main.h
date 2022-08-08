ecf08f116921b1291a91aad5ac019d3e2fe72855
Paul Okoli <75359053+PaulPextra@users.noreply.github.com>
2022-08-08 12:55:50 +0100

    Update main.h
    Updating main.h file

--- main.h
+++ main.h
@@ -1,19 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <stdio.h>
 
-typedef struct print
-{
-  char *t;
-  int (*f)(va_list);
-}print_t;
-
-int _printf(const char *format, ...);
 int _putchar(char c);
-int format_c(va_list valist, char *buffer, int *index);
-int format_s(va_list valist, char *buffer, int *index);
-int print_i(va_list i);
-
   
 #endif /* MAIN_H */
